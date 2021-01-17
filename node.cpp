39fa1d077a0f8bfbe1d3990d257c81306e1996cb
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2021-01-16 17:27:34 -0800

    Create node.cpp

---
+++ node.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include "node.h"
+
+using namespace std;
+
+Node::Node() 
+{
+  //constructor
+  data = 0;
+  right = NULL;
+  left = NULL;
+  color = 1; //start red
+  parent = NULL;
+}
+
+Node::~Node() 
+{
+  //deleter
+  right = NULL;
+  left = NULL;
+  parent = NULL;
+}
+
+//setters
+void Node::setData(int newD) 
+{
+  data = newD;
+}
+
+void Node::setRight(Node* newR) 
+{
+  right = newR;
+}
+
+void Node::setLeft(Node* newL) 
+{
+  left = newL;
+}
+
+void Node::setColor(int newC) 
+{
+  color = newC;
+}
+
+void Node::setParent(Node* newP) 
+{
+  parent = newP;
+}
+
+//getters
+int Node::getData() 
+{
+  return data;
+}
+
+Node* Node::getRight() 
+{
+  return right;
+}
+
+Node* Node::getLeft() 
+{
+  return left;
+}
+
+int Node::getColor() 
+{
+  return color;
+}
+
+Node* Node::getParent() 
+{
+  return parent;
+}
+© 2021 GitHub, Inc.
